7d0767e646dc2ea5d1d7c8970b60d86623a75e92
coffee <coffee@kakadeMacBook-Pro.local>
2022-03-10 17:22:33 +0700

    feat: 端口证书配置

---
+++ config/app.go
@@ -0,0 +1,17 @@
+package config
+
+// import (
+//     "github.com/owner888/kaligo/config"
+// )
+//
+// func init() {
+//     config.Add("app", config.StrMap{
+//         "server": map[string]interface{}{
+//             // 监听地址
+//             "port": ":9090",
+//             // 非TLS 下面两个留空即可
+//             "crtfile": "/etc/nginx/ssl/kaligo.com.crt",
+//             "keyfile": "/etc/nginx/ssl/kaligo.com.key",
+//         },
+//     })
+// }
