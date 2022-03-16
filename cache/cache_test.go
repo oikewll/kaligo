6f31933383956f62aeb2da15baa4c5184ccb383e
Cyril <cyril8239@gmail.com>
2022-03-16 16:17:10 +0700

    test: cache add test

--- cache/cache_test.go
+++ cache/cache_test.go
@@ -1,23 +1,18 @@
 package cache
 
 import (
-    "testing"
-    "time"
+	"testing"
 
-    "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestMemoryCache(t *testing.T) {
-    memory := NewMemory()
-    memory.Set("name", "kaka", 10)
-    name := memory.Get("name")
-    t.Logf("name: [ %v ]", name)
-}
-
 func TestCache(t *testing.T) {
-    cache := New("memory")
-    cache.Set("name", "kaka", time.Millisecond)
-    assert.Equal(t, "kaka", cache.Get("name"))
-    time.Sleep(time.Millisecond * 2)
-    assert.Nil(t, cache.Get("name"))
+	cache := New("redis")
+	assert.IsType(t, &Redis{}, cache)
+	cache = New("memcache")
+	assert.IsType(t, &Memcache{}, cache)
+	cache = New("memory")
+	assert.IsType(t, &Memory{}, cache)
+	cache = New("")
+	assert.IsType(t, &Memory{}, cache)
 }
--- cache/memory_test.go
+++ cache/memory_test.go
@@ -1,12 +1,39 @@
 package cache
 
 import (
+	"os"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
+var cache Memory
+
+func TestMain(m *testing.M) {
+	cache = *NewMemory()
+	os.Exit(m.Run())
+}
+
+func TestMemoryCache(t *testing.T) {
+	assert.NotNil(t, cache)
+	assert.Nil(t, cache.Get("unknown_key"))
+
+	key := "any_key"
+	value := "any value"
+	cache.Set(key, value, time.Millisecond)
+	assert.Equal(t, value, cache.Get(key))
+
+	cache.Delete(key)
+	assert.Nil(t, cache.Get(key))
+
+	cache.Set(key, value, time.Millisecond)
+	assert.Equal(t, value, cache.Get(key))
+	time.Sleep(time.Millisecond * 2)
+	assert.Nil(t, cache.Get(key))
+}
+
 func BenchmarkMemoryGet(b *testing.B) {
-	cache := NewMemory()
 	cache.Set("key", time.Now(), time.Minute)
 	for i := 0; i < b.N; i++ {
 		_ = cache.Get("key")
@@ -14,7 +41,6 @@ func BenchmarkMemoryGet(b *testing.B) {
 }
 
 func BenchmarkMemorySet(b *testing.B) {
-	cache := NewMemory()
 	for i := 0; i < b.N; i++ {
 		_ = cache.Set("key", time.Now(), time.Minute)
 	}
