aa36056d80b57f58031622dc2b82a564c5a02114
coffee <coffee@gmail.com>
2022-03-17 11:11:58 +0700

    fix: 修改超时链接类型为int，单位为秒

--- cache/redis.go
+++ cache/redis.go
@@ -28,8 +28,8 @@ func NewRedis(opts *RedisOpts) *Redis {
 	pool := &redis.Pool{
 		MaxIdle:     opts.MaxIdle,                                  // 最大空闲连接数
 		MaxActive:   opts.MaxActive,                                // 最大连接数
-		IdleTimeout: time.Second * time.Duration(opts.IdleTimeout), // 闲连接超时时间
-        Wait:        true,                                          // 超过最大连接数的操作:等待
+		IdleTimeout: time.Second * time.Duration(opts.IdleTimeout), // 空闲连接超时时间(单位: 秒)
+        Wait:        opts.Wait,                                     // 超过最大连接数的操作:等待
 		Dial: func() (redis.Conn, error) {
             c, err := redis.Dial("tcp", opts.Host,
 				redis.DialDatabase(opts.Database),
--- config/cache.go
+++ config/cache.go
@@ -1,7 +1,6 @@
 package config
 
 // import (
-//     "time"
 //     "github.com/owner888/kaligo/config"
 // )
 //
@@ -12,10 +11,10 @@ package config
 //             "port"          : 6379,
 //             "password"      : "",
 //             "database"      : 0,
-//             "max_idle"      : 10,                // 最大空闲连接数
-//             "max_active"    : 200,               // 最大连接数
-//             "idle_timeout"  : 180 * time.Second, // 空闲连接超时时间
-//             "wait"          : true,              // 超过最大连接数的操作:等待
+//             "max_idle"      : 10,        // 最大空闲连接数
+//             "max_active"    : 200,       // 最大连接数
+//             "idle_timeout"  : 180,       // 空闲连接超时时间(单位: 秒)
+//             "wait"          : true,      // 超过最大连接数的操作:等待
 //         },
 //         "memcache": map[string]interface{}{
 //             "host": []string{"10.0.0.1:11211", "10.0.0.2:11211", "10.0.0.3:11211"},
