944295b0a43906bcb06d42ae1d78d8b54e8fd3ba
Cyril <cyril8239@gmail.com>
2022-03-22 14:05:16 +0700

    feat: Params 参数支持自定义默认值

--- tree.go
+++ tree.go
@@ -11,20 +11,28 @@ type Param struct {
 // It is therefore safe to read values by the index.
 type Params []Param
 
+// ?name=test&age=10
+// ?age=10
+// ?name=&age=10
+// Get("name", "kk")
 // Get returns the value of the first Param which key matches the given name and a boolean true.
 // If no matching Param is found, an empty string is returned and a boolean false .
-func (ps Params) Get(name string) (string, bool) {
+func (ps Params) Get(name string, defalutValue ...string) (string, bool) {
     for _, entry := range ps {
         if entry.Key == name {
             return entry.Value, true
         }
     }
-    return "", false
+    var ret string = ""
+    if len(defalutValue) != 0 {
+        ret = defalutValue[0]
+    }
+    return ret, false
 }
 
 // ByName returns the value of the first Param which key matches the given name.
 // If no matching Param is found, an empty string is returned.
-func (ps Params) ByName(name string) (va string) {
-    va, _ = ps.Get(name)
+func (ps Params) ByName(name string, defalutValue ...string) (va string) {
+    va, _ = ps.Get(name, defalutValue...)
     return
 }
--- tree_test.go
+++ tree_test.go
@@ -10,7 +10,10 @@ func TestParams(t *testing.T) {
     params := Params{
         Param{"key1", "value1"},
         Param{"key2", "value2"},
+        Param{"key3", ""},
     }
     assert.Equal(t, "value1", params.ByName("key1"))
-    assert.Equal(t, "", params.ByName("key3"))
+    assert.Equal(t, "", params.ByName("key3", "defaultValue"))
+    assert.Equal(t, "defaultValue", params.ByName("unknown_key", "defaultValue"))
+    assert.Equal(t, "", params.ByName("unknown_key"))
 }
