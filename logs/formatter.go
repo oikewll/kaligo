ab8feacfff3786719ef7e18f91b0f1f63679cfc4
Cyril <cyril8239@gmail.com>
2022-04-01 21:48:13 +0700

    feat: log 优化 level 输出标志

--- logs/formatter.go
+++ logs/formatter.go
@@ -3,30 +3,30 @@ package logs
 import "fmt"
 
 type Formatter interface {
-	Printf(level Level, format string, a ...any) string
+    Printf(level Level, format string, a ...any) string
 }
 
 type PlainFormatter struct {
 }
 
 func (f *PlainFormatter) Printf(level Level, format string, a ...any) string {
-	return fmt.Sprintf(format, a...) + "\n"
+    return fmt.Sprintf(format, a...) + "\n"
 }
 
 type ConsoleFormatter struct {
 }
 
 func (f *ConsoleFormatter) Printf(level Level, format string, a ...any) string {
-	levelStr := ""
-	switch level {
-	case LevelDebug:
-		levelStr = WhiteLight + "[D] " + Reset
-	case LevelInfo:
-		levelStr = "[I] "
-	case LevelWarn:
-		levelStr = Yellow + "[W] " + Reset
-	case LevelError:
-		levelStr = Red + "[E] " + Reset
-	}
-	return levelStr + fmt.Sprintf(format, a...)
+    levelStr := ""
+    switch level {
+    case LevelDebug:
+        levelStr = BlackLight + "Ⓓ DEBUG " + Reset
+    case LevelInfo:
+        levelStr = "ⓘ INFO  "
+    case LevelWarn:
+        levelStr = Yellow + "ⓦ WARN  " + Reset
+    case LevelError:
+        levelStr = Red + "ⓧ ERROR " + Reset
+    }
+    return levelStr + fmt.Sprintf(format, a...)
 }
