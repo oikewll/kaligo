8e53bdb56cfc5195ab18c5ffefa25657972af847
dawson <dawsonhoohi@gmail.com>
2022-05-27 01:28:30 +0800

    增加表单验证规则

--- uiframe/pages/user/index.vue
+++ uiframe/pages/user/index.vue
@@ -1,12 +1,30 @@
 <template>
     <Wrap :title="getFormData.name" fillout>
         <template slot="content">
-            <el-form ref="formSubmit" :model="formData" :rules="formRules" label-width="80px">
-                <el-form-item :label="item.title" v-for="(item, idx) in getFormData.components" :key="idx">
-                    <el-input v-model="formData[item.field]" :type="item.type"></el-input>
+            <el-form
+                ref="formSubmit"
+                :model="formData"
+                :rules="formRules"
+                label-width="80px"
+            >
+                <el-form-item
+                    :label="item.title"
+                    v-for="(item, idx) in getFormData.components"
+                    :key="idx"
+                    :prop="item.field"
+                >
+                    <el-input
+                        v-model="formData[item.field]"
+                        :type="item.type"
+                        @keyup.enter.native="onSubmitForm"
+                        :placeholder="item.placeholder"
+                    ></el-input>
                 </el-form-item>
                 <el-form-item>
-                    <el-button type="primary" @click="onSubmitForm"
+                    <el-button
+                        type="primary"
+                        @click="onSubmitForm"
+                        :loading="formLoading"
                         >立即创建</el-button
                     >
                     <el-button @click="history.back()">取消</el-button>
@@ -25,38 +43,58 @@ export default {
     data: () => {
         return {
             getFormData: {
-                name: '',
+                name: "",
             },
             formData: {},
-            formRules: {},
+            formRules: {
+                username: [
+                    {
+                        required: true,
+                        message: "请输入用户名",
+                        trigger: "change",
+                    },
+                ],
+                password: [
+                    {
+                        required: true,
+                        message: "请输入密码",
+                        trigger: "change",
+                    },
+                ],
+            },
+            formLoading: false,
         };
     },
     computed: {},
     async created() {
         const { data } = await this.$api.user.getForm();
-        console.log(data.data);
         this.getFormData = data.data;
 
         let formRules = {};
-        data.data.components.forEach(item=>{
-            formRules[item.field] = [Object.assign({trigger: "change"}, item.validate)]
-        })
+        data.data.components.forEach((item) => {
+            formRules[item.field] = [
+                Object.assign({ trigger: "change" }, item.validate),
+            ];
+        });
         this.formRules = formRules;
     },
     methods: {
-        async onSubmitForm(){
+        async onSubmitForm() {
+            this.formLoading = true;
             this.$refs.formSubmit.validate(async (valid) => {
-                return console.warn(valid)
                 if (valid) {
-                    const {getFormData, formData} = this;
-                    const rs = await this.$axios[getFormData['method'].toLocaleLowerCase()](getFormData['path'], formData);
+                    const { getFormData, formData } = this;
+                    const rs = await this.$axios[
+                        getFormData["method"].toLocaleLowerCase()
+                    ](getFormData["path"], formData);
                     if (rs) {
                         this.$message.success("Successfully");
                         // this.$router.push("/");
                     }
                 }
+                this.formLoading = false;
             });
-        }
+        },
     },
 };
 </script>
